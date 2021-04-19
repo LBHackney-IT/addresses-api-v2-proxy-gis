d709f6a77397bf8e15014f4dbad127e924c3b7a5
Sandrine Balley <sandrine.balley@hackney.gov.uk>
2021-04-19 16:45:13 +0100

    allowed origin different in staging and prod

--- address-proxy.js
+++ address-proxy.js
@@ -50,7 +50,7 @@ const API_ENDPOINT =
       headers: {
         // 'Access-Control-Allow-Origin': '*',
         //'Access-Control-Allow-Origin': 'http://localhost:1234',
-        'Access-Control-Allow-Origin': 'https://map2.hackney.gov.uk',
+        'Access-Control-Allow-Origin': process.env.ALLOWED_ORIGIN,
         'Access-Control-Allow-Credentials': false
       },
       body: JSON.stringify({
