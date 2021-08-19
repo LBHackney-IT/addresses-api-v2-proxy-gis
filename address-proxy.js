c190f00a3a65f90b133ffda398a4ef30acd5aeca
Sandrine Balley <sandrine.balley@hackney.gov.uk>
2021-08-19 13:54:21 +0100

    test cors

--- address-proxy.js
+++ address-proxy.js
@@ -4,55 +4,56 @@ const fetch = require("node-fetch");
 const API_ENDPOINT =
   "https://6kb2p9kgb0.execute-api.eu-west-2.amazonaws.com/production/api/v2/addresses";
 
-  exports.handler = async (event, context) => {
-    let response;
-    
-    let request = `${API_ENDPOINT}`;
-    if (event.queryStringParameters){
-      if (event.queryStringParameters.format){
-        request = request + `?format=${event.queryStringParameters.format}`;
-      }
-      else{
-        request = request + `?format=simple`;
-      }
-      if (event.queryStringParameters.uprn){
-        request = request + `&uprn=${event.queryStringParameters.uprn}`;
-      }
-      if (event.queryStringParameters.postcode){
-        request = request + `&postcode=${event.queryStringParameters.postcode}`;
-      }
-      if (event.queryStringParameters.page){
-        request = request + `&page=${event.queryStringParameters.page}`;
-      }
-      if (event.queryStringParameters.usage_primary){
-        request = request + `&usage_primary=${event.queryStringParameters.usage_primary}`;
-      }
-      else{
-        request = request + `&usage_primary=residential,commercial,dual use,land`;
-      }
-      if (event.queryStringParameters.address_status){
-        request = request + `&address_status=${event.queryStringParameters.address_status}`;
-      }
-      if (event.queryStringParameters.query){
-        request = request + `&query=${event.queryStringParameters.query}`;
-      }
+exports.handler = async (event, context) => {
+  let response;
+  let request = `${API_ENDPOINT}`;
+
+  if (event.queryStringParameters){
+    if (event.queryStringParameters.format){
+      request = request + `?format=${event.queryStringParameters.format}`;
     }
-  
-    try {
-      response = await fetch(request, {
-        headers: {
-            Authorization: process.env.TOKEN,
-        },
-      });
-    } catch (err) {
-      return {
-        statusCode: err.statusCode || 500,
-        body: JSON.stringify({
-          error: err.message,
-        }),
-      };
+    else{
+      request = request + `?format=simple`;
     }
-  
+    if (event.queryStringParameters.uprn){
+      request = request + `&uprn=${event.queryStringParameters.uprn}`;
+    }
+    if (event.queryStringParameters.postcode){
+      request = request + `&postcode=${event.queryStringParameters.postcode}`;
+    }
+    if (event.queryStringParameters.page){
+      request = request + `&page=${event.queryStringParameters.page}`;
+    }
+    if (event.queryStringParameters.usage_primary){
+      request = request + `&usage_primary=${event.queryStringParameters.usage_primary}`;
+    }
+    else{
+      request = request + `&usage_primary=residential,commercial,dual use,land`;
+    }
+    if (event.queryStringParameters.address_status){
+      request = request + `&address_status=${event.queryStringParameters.address_status}`;
+    }
+    if (event.queryStringParameters.query){
+      request = request + `&query=${event.queryStringParameters.query}`;
+    }
+  }
+
+  try {
+    response = await fetch(request, {
+      headers: {
+          Authorization: process.env.TOKEN,
+      },
+    });
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    };
+  }
+
+  if (false){
     return {
       statusCode: 200,
       headers: {
@@ -63,4 +64,16 @@ const API_ENDPOINT =
         data: await response.json(),
       }),
     };
-  };
+  }
+  else {
+    return{
+      statusCode: 401,
+      headers: {
+        'Access-Control-Allow-Origin': 'xxx',
+        'Access-Control-Allow-Credentials': false
+      },
+      body: 'forbidden'
+    }
+  }
+  
+};
